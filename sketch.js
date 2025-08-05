92902a0c5147951c75b5b41c0aa610bcc4431061
perolip <joao.peroli@escola.pr.gov.br>
2025-08-05 09:59:33 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,12 @@
 function setup() {
-  createCanvas(1920, 1080, WEBGL); //plano 3D
-  terra=loadImage('terra.jpg')
+  createCanvas(1920, 1080, WEBGL);
+  terra=loadImage('terra.jpg')
 }
 
 function draw() {
-  background(205, 102, 94); //cor de fundo
-  rotateY(millis()/1000); // rotação da terra
-  texture(terra); //textura da terra, imagem.
-  sphere(80, 100); //esfera 3D
-  
-  
+  background(0, 0, 0); // Fundo preto
+  rotateY(millis()/6000);
+  texture(terra);
+  sphere(150); // Esfera maior
+  
 }
